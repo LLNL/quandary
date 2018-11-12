6a0c042724f89eb53d5b7e72cc6f65ca7abf04be
Anders Petersson <petersson1@llnl.gov>
2018-11-11 21:38:16 -0800

    Working on the hamiltonian simulator

--- octave/hamsim.m
+++ octave/hamsim.m
@@ -14,7 +14,7 @@
 % u: solution vector at every time step
 %
 function [uTime] = hamsim(H, bvec, verbose, cfl)
-
+  filterTime = 1;
   abs_or_real=0; # plot the abs of the solution (1 for real)
 
   if nargin < 1
@@ -51,10 +51,7 @@ function [uTime] = hamsim(H, bvec, verbose, cfl)
   printf("Dimensions look ok...\n");
 #  return;
   
-  N = n1; # vector dimension
-
-# final time
-  T = 50;
+  Nrow = n1; # vector dimension
 
 # estimate largest eigenvalue
 
@@ -64,34 +61,33 @@ function [uTime] = hamsim(H, bvec, verbose, cfl)
     printf("(maxeig) = (%e)\n", maxeig);
   end
 
-  cfunc = zeros(N,1);
-  beta = zeros(N,1);
-  cu_sp = zeros(N,1);
-  ca1 = zeros(N,1);
-  vect = zeros(N,1);
-				# Final time T
+# final time
+  Tfinal = 50;
+
+# time step
   dt = cfl/maxeig; # largest eigenvalue of H0 = d3, H0+poly*H1 estimated by maxeig
-  nsteps = ceil(T/dt)+1;
-  dt = T/(nsteps-1);
+  nsteps = ceil(Tfinal/dt)+1;
+  dt = Tfinal/(nsteps-1);
   if (verbose)
     printf("Final time = %e, number of time steps = %d, max eigenvalue = %e, cfl = %e, time step = %e\n", ...
-	   T, nsteps, maxeig, cfl, dt);
+	   Tfinal, nsteps, maxeig, cfl, dt);
   end
 
 # allocation of solution vectors
-  uTime = zeros(N,nsteps);
+  uTime = zeros(Nrow,nsteps);
 
 # initial data 
   uTime(:,1) = bvec;
   
 # 2nd order Taylor expansion to t = +dt
 
-  uTime(:,2) = uTime(:,1) + dt * I * H *uTime(:,1) + 0.5*dt^2 * (-H*H*uTime(:,2));
+  uTime(:,2) = bvec + dt * I * H *bvec - 0.5*dt^2 * (H*H*bvec) ;
+#  uTime(:,2) = bvec + dt * I * H *bvec - 0.5*dt^2 * (H*H*bvec) - I*(dt^3)/6 * H*H*H*bvec ;
 # adding another term in the expansion generates more wiggles
 
 # for computing the energy
-  v1 = zeros(N,1);
-  v2 = zeros(N,1);
+  v1 = zeros(Nrow,1);
+  v2 = zeros(Nrow,1);
 
   t=dt;
   step=1;
@@ -123,26 +119,61 @@ function [uTime] = hamsim(H, bvec, verbose, cfl)
     end # if verbose
   end # time stepping loop
 
-  df = 1/T;
+  Tperiod = Tfinal+dt;
+  df = 1/Tperiod;
   Nf = nsteps;
+  td = dt*[0:nsteps-1];
+
+# Filter the numerical solution to get rid of small high wavenumber oscillations
+% optionally filter
+  if (filterTime)
+    fc = 0.5*Nf;
+    [b a]=mybutter2(2*dt*fc);
+    uFilt = zeros(Nrow,nsteps);
+    for k=1:Nrow
+      uFilt(k,:)= myfiltfilt(b,a,uTime(k,:));
+    end
+
+    figure(3);
+    h = plot(td, uTime(1,:),'b', td, uFilt(1,:),'r--');
+    set(h,"linewidth",2);
+    title("Time domain");
+    legend("leap-frog", "Filtered");
+    uTime = uFilt;
+  end
+  
+
+	     # Window the time response before Fourier transforming it
+#  wind = ones(1,nsteps);
+#  wind = sin(pi*td/Tperiod);
+  wind = exp(-( (td-0.5*Tperiod)/(0.5*Tperiod/4) ).^2);
+  uWind = zeros(Nrow, nsteps);
+  for (k=1:Nrow)
+    uWind(k,:) = uTime(k,:).*wind;
+  end
+  
   freq=[ -(ceil((Nf-1)/2):-1:1), 0, (1:floor((Nf-1)/2)) ] * df; # In Hz
   om = 2*pi*freq; % angular frequency
 # Discrete Fourier transformation of the time traces
-  uOmega = fftshift(fft(ifftshift(uTime),Nf,2));
+#  uOmega = fftshift(fft(ifftshift(uWind),Nf,2))/Nf;
+  uOmega = fftshift(fft(uWind,Nf,2))/Nf;
+
+				# find the eigenvalues
+  uAvg = sqrt(sumsq(uOmega, 1)/Nrow);
   
   if (verbose)
     figure(1);
-    td = dt*[0:nsteps-1];
-    h = plot(td, uTime(1,:),'b', td, uTime(2,:),'r');
+    h = plot(td,wind,'k',td, uWind(1,:),'b', td, uWind(2,:),'r--');
     set(h,"linewidth",2);
     title("Time domain");
-    legend("u0", "u1","location","north");
+    legend("window","u0", "u1","location","northeast");
 
     figure(2);
-    h = plot( om, abs(uOmega(1,:)), 'm+',  om, abs(uOmega(2,:)), 'cd');
+    h = semilogy( om, uAvg, 'm+');
+    
     set(h,"linewidth",2);
     set(h,"markersize",3);
-    legend("Abs(u0)", "Abs(u1)", "location", "east");
+    legend("SumSq(uOmega)");
     title("Frequency domain");
     
   end
