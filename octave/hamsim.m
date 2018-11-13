7415e37ac46168886a50cd64adb427727307e730
Anders Petersson <petersson1@llnl.gov>
2018-11-12 21:33:11 -0800

    Trying to back out the eigenvectors

--- octave/hamsim.m
+++ octave/hamsim.m
@@ -18,17 +18,19 @@ function [uTime] = hamsim(H, bvec, verbose, cfl)
   firstOrder = 1;
   leapFrog = 0;
   magnus = 1;
+  rk4 = 0;
   abs_or_real=0; # plot the abs of the solution (1 for real)
 
-  U=[-1, -1; -1, 1]/sqrt(2);
-  Lambda=[-1 , 0; 0, 10];
+#  U=[1, 0; 0, 1];
+  U=[1, 1; -1, 1]/sqrt(2);
+  Lambda=[1 , 0; 0, 2];
 
   H = U*Lambda*U';
 
   H2 = H*H;
 
   if nargin < 2
-    bvec = [2;1];
+    bvec = [1;2];
   end
 
   if nargin < 3
@@ -36,7 +38,7 @@ function [uTime] = hamsim(H, bvec, verbose, cfl)
   end
   
   if nargin < 4
-    cfl = 1;
+    cfl = 0.5; #depends on the method
   end
 
   [n1 n2] = size(H);
@@ -68,7 +70,7 @@ function [uTime] = hamsim(H, bvec, verbose, cfl)
   end
 
 # final time
-  Tfinal = 50;
+  Tfinal = 100;
 
 # time step
   dt = cfl/maxeig; # largest eigenvalue of H0 = d3, H0+poly*H1 estimated by maxeig
@@ -102,9 +104,10 @@ function [uTime] = hamsim(H, bvec, verbose, cfl)
   if (verbose)
     v1 = uTime(:,step+1) + uTime(:,step);
     v2 = uTime(:,step+1) - uTime(:,step);
-    energy = 0.25*(norm(v1)^2 - norm(v2)^2);
+    energy1 = 0.25*(norm(v1)^2 - norm(v2)^2);
+    energy = uTime(:,1)'*H*uTime(:,1);
 
-    printf("Time step = %d, time = %e, energy = %e\n", step, t, energy);
+    printf("Time step = %d, time = %e, energy1 = %e, energy = %e\n", step, t, energy1, energy);
   end # if verbose
 
 # time stepping loop, harmonic oscillator, 1st order formulation
@@ -131,7 +134,7 @@ function [uTime] = hamsim(H, bvec, verbose, cfl)
       for row=1:Nrow
 	expH(row,row) = exp(I*dt*Lambda(row,row));
       end
-      expH
+#      expH
       for step=1:nsteps-1
 	uTime(:,step+1) = U*expH*U' * uTime(:,step);
 
@@ -142,13 +145,13 @@ function [uTime] = hamsim(H, bvec, verbose, cfl)
 	    v1 = uTime(:,step+1) + uTime(:,step);
 	    v2 = uTime(:,step+1) - uTime(:,step);
 	    energy = 0.25*(norm(v1)^2 - norm(v2)^2);
+	    energy = uTime(:,step+1)'*H*uTime(:,step+1);
 
 	    printf("Time step = %d, time = %e, energy = %e\n", step, t, energy);
 	  end
 	end # if verbose
       end # time stepping loop
-    else
-				# RK-4
+    elseif (rk4) # RK-4		
       k1 = zeros(Nrow,1);
       k2 = zeros(Nrow,1);
       k3 = zeros(Nrow,1);
@@ -174,6 +177,9 @@ function [uTime] = hamsim(H, bvec, verbose, cfl)
 	  end
 	end # if verbose
       end # time stepping loop
+    else
+      printf("ERROR: Undefined time-integration method\n");
+      return;
     end # RK-4
   else
 # second order formulation
@@ -214,16 +220,16 @@ function [uTime] = hamsim(H, bvec, verbose, cfl)
     end
 
     figure(3);
-    h = plot(td, uFilt(1,:),'b', td, uFilt(2,:),'r--');
-    set(h,"linewidth",2);
+    h = plot(td, uFilt(1,:),'b', td, uFilt(2,:),'r-');
+    set(h,"linewidth",1.5);
     title("Filtered Time domain");
 #    uTime = uFilt;
   end
   
 
 	     # Window the time response before Fourier transforming it
-  wind = ones(1,nsteps);
-#  wind = sin(pi*td/Tperiod);
+#  wind = ones(1,nsteps);
+  wind = sin(pi*td/Tperiod);
 #  wind = exp(-( (td-0.5*Tperiod)/(0.5*Tperiod/4) ).^2);
   uWind = zeros(Nrow, nsteps);
   for (k=1:Nrow)
@@ -233,26 +239,56 @@ function [uTime] = hamsim(H, bvec, verbose, cfl)
   freq=[ -(ceil((Nf-1)/2):-1:1), 0, (1:floor((Nf-1)/2)) ] * df; # In Hz
   om = 2*pi*freq; % angular frequency
 # Discrete Fourier transformation of the time traces
-#  uOmega = fftshift(fft(ifftshift(uWind),Nf,2))/Nf;
-  uOmega = fftshift(fft(uWind,Nf,2))/Nf;
+  uOmega = fftshift(fft(ifftshift(uWind),Nf,2))/Nf;
 
 				# find the eigenvalues
   uAvg = sqrt(sumsq(uOmega, 1)/Nrow);
-  
-  if (verbose)
-    figure(1);
-    h = plot(td, wind, 'k', td, uWind(1,:),'b', td, uWind(2,:),'r--');
-    set(h,"linewidth",2);
-    title("Windowed, time domain");
-
-    figure(2);
-    h = semilogy( om, uAvg, 'm+');
-    
-    set(h,"linewidth",2);
-    set(h,"markersize",3);
-    legend("SumSq(uOmega)");
-    title("Windowed, frequency domain");
-    
+  maxPnts=zeros(Nrow,1);
+  numMaxPnts=0;
+  for k=2:nsteps-1
+    dp = uAvg(k+1) - uAvg(k);
+    dm = uAvg(k) - uAvg(k-1);
+    if (dp*dm < 0 && dm>0)
+      numMaxPnts = numMaxPnts+1;
+      maxPnts(numMaxPnts)=k;
+    end
   end
 
+  for q=1:numMaxPnts
+    printf("Max at q=%d, freq(q)=%e\n", maxPnts(q), om(maxPnts(q)));
+    printf("uOmega: "); uOmega(:,maxPnts(q))
+  end
+
+				# try to reconstruct the time function
+  uRec=zeros(Nrow, nsteps);
+  for q=1:numMaxPnts
+    lambda = om(maxPnts(q));
+    amp = uOmega(:, maxPnts(q));
+    for row=1:Nrow
+      uRec(row,:) = uRec(row,:) + amp(row)*exp(I*lambda*td);
+    end
+  end
+  
+  figure(1);
+  h = plot(td, wind, 'k', td, real(uTime(1,:)),'b', td, imag(uTime(1,:)),'r',...
+	   td, real(uRec(1,:)),'c--', td, imag(uRec(1,:)),'m--');
+  set(h,"linewidth",1.5);
+  set(h,"markersize",3);
+  title("1st comp, real+imag part, orig+recon, time domain");
+
+  figure(2);
+  h = plot(td, wind, 'k', td, real(uTime(2,:)),'b', td, imag(uTime(2,:)),'r',...
+	   td, real(uRec(2,:)),'c--', td, imag(uRec(2,:)),'m--');
+  set(h,"linewidth",1.5);
+  set(h,"markersize",3);
+  title("2nd comp, real+imag part, orig+recon, time domain");
+
+  figure(3);
+  h = semilogy( om, uAvg, 'm-');
+    
+  set(h,"linewidth",1.5);
+  set(h,"markersize",3);
+  legend("SumSq(uOmega)");
+  title("Windowed, frequency domain");
+    
 end
