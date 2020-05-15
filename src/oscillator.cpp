27c396ec51679c65a37c1ca06afc706e499e2094
Stefanie Guenther <guenther5@llnl.gov>
2020-05-14 18:01:14 -0700

    Expected energy for colocated x.

--- src/oscillator.cpp
+++ src/oscillator.cpp
@@ -179,8 +179,9 @@ double Oscillator::expectedEnergy(Vec x) {
   for (int i=0; i<dimmat; i++) {
     /* Get diagonal element in number operator */
     MatGetValue(NumberOP, i, i, &num_diag);
-    /* Get diagonal element in rho */
+    /* Get diagonal element in rho (real) */
     int idx_diag = i * dimmat + i;
+    idx_diag = 2*idx_diag;
     VecGetValues(x, 1, &idx_diag, &xdiag);
     expected += num_diag * xdiag;
   }
