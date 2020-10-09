e191449cf8943069d586f181a22f9279b2cd35dc
Stefanie Guenther <guenther5@llnl.gov>
2020-10-08 22:34:49 -0700

    Adapt switch.py for python3.7.

--- util/switch.py
+++ util/switch.py
@@ -37,7 +37,8 @@ class switch(object):
     def __iter__(self):
         """Return the match method once, then stop"""
         yield self.match
-        raise StopIteration
+        #raise StopIteration
+        return 
     
     def match(self, *args):
         """Indicate whether or not to enter a case suite"""
