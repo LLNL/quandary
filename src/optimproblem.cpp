1d300b20fa0582abb56bb0ba12e6cb2562b5b5ad
Stefanie Guenther <guenther5@llnl.gov>
2021-11-18 15:51:53 -0800

    Init optim params s.t. 0 = control(0)=control(T).
    For each oscillator, the control functions should start and end at 0.0.
    The optimizer ensures that through the bounds, however the initial guess
    did not yet satisfy this when initializing either randomly or with
    constant amplitude. Now, the first and last two spline coefficients are
    always set to zero so that the control functions start and end at zero
    always.

--- src/optimproblem.cpp
+++ src/optimproblem.cpp
@@ -625,6 +625,20 @@ void OptimProblem::getStartingPoint(Vec xinit){
     delete [] vecread;
   }
 
+  /* for the first and last two splines, overwrite the parameters with zero to ensure that control at t=0 and t=T is zero. */
+  PetscInt col = 0.0;
+  for (int iosc = 0; iosc < timestepper->mastereq->getNOscillators(); iosc++){
+    int ncarrier = timestepper->mastereq->getOscillator(iosc)->basisfunctions->carrier_freq.size();
+    PetscInt ibegin = 2*2*ncarrier;
+    PetscInt iend = (timestepper->mastereq->getOscillator(iosc)->getNSplines()-2)*2*ncarrier;
+
+    for (int i = 0; i < timestepper->mastereq->getOscillator(iosc)->getNParams(); i++) {
+      if (i < ibegin || i >= iend) VecSetValue(xinit, col, 0.0, INSERT_VALUES);
+      col++;
+    }
+  }
+
+
   /* Assemble initial guess */
   VecAssemblyBegin(xinit);
   VecAssemblyEnd(xinit);
