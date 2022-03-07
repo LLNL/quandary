2a7f56c39f31eb463a92280749ea3469a00a0637
Stefanie Guenther <guenther5@llnl.gov>
2022-03-07 15:09:42 -0800

    Bugfix for Schroedinger gradient with Jmeasure.
    Initialize ilo and ihi when accessing Petsc Vector in J_diff.

--- src/optimtarget.cpp
+++ src/optimtarget.cpp
@@ -338,6 +338,7 @@ void OptimTarget::evalJ_diff(const Vec state, Vec statebar, const double Jbar){
           diagID_im = getIndexImag(i);
           rhoii_re = 0.0;
           rhoii_im = 0.0;
+          VecGetOwnershipRange(state, &ilo, &ihi);
           if (ilo <= diagID_re && diagID_re < ihi) VecGetValues(state, 1, &diagID_re, &rhoii_re);
           if (ilo <= diagID_im && diagID_im < ihi) VecGetValues(state, 1, &diagID_im, &rhoii_im);
           if (ilo <= diagID_re && diagID_re < ihi) VecSetValue(statebar, diagID_re, 2.*Jbar*lambdai*rhoii_re, ADD_VALUES);
