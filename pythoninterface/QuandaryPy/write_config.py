3bf3f9f230b893c61afc149fbda705360cafd43e
Stefanie Guenther <guenther5@llnl.gov>
2023-05-24 18:42:18 -0700

    Create package and test write_config.py within main.py

--- pythoninterface/QuandaryPy/write_config.py
+++ pythoninterface/QuandaryPy/write_config.py
@@ -1,9 +1,17 @@
-def write_Quandary_config_file(configfilename, Nt, Ne, T, nsteps, freq01, rotfreq, selfkerr, couple_coeff, couple_type, D1, carrierfreq, gatefilename, initialpcof_filename, optim_bounds, inftol, maxiter, tik0, leakage_weights, print_frequency_iter, runtype="optimization", gamma_dpdm=0.0, final_objective=1, gamma_energy=0.0, splines_real_imag=True, phase_scaling_factor=1.0, datadir="./"):
+import numpy as np
+
+def write_Quandary_config_file(Ne, Ng, T, nsteps, freq01, rotfreq, selfkerr, crosskerr, Jkl, T1, T2, D1, carrierfreq, gatefilename, initialpcof_filename, optim_bounds, inftol, maxiter, tik0, leakage_weights, print_frequency_iter, runtype, gamma_dpdm, final_objective, gamma_energy, *, datadir=".", configfilename="config.cfg"):
     # final_objective = 1 uses the trace infidelity;
     # final_objective = 2 uses the Frobenius norm squared
+    # final_objective = 3 for the Jmeasure 
     # gamma_dpdm > 0 to suppress 2nd time-derivative of the population
-    assert final_objective == 1 or final_objective == 2
 
+
+    # TODO: 
+    #  * defaults
+    #  * Create data dir if does not exists!
+
+    Nt = [Ne[i] + Ng[i] for i in range(len(Ng))]
     mystring = "nlevels = " + str(Nt)[1:-1] + "\n"
     mystring += "nessential= " + str(Ne)[1:-1] + "\n"
     mystring += "ntime = " + str(nsteps) + "\n"
@@ -11,33 +19,52 @@ def write_Quandary_config_file(configfilename, Nt, Ne, T, nsteps, freq01, rotfre
     mystring += "transfreq = " + str(freq01)[1:-1] + "\n"
     mystring += "rotfreq= " + str(rotfreq)[1:-1] + "\n"
     mystring += "selfkerr = " + str(selfkerr)[1:-1] + "\n"
-    if couple_type == 1:
-        mystring += "crosskerr= " + str(couple_coeff)[1:-1] + "\n"
-        mystring += "Jkl= 0.0\n"
+    if len(crosskerr)>0:
+        mystring += "crosskerr= " + str(crosskerr)[1:-1] + "\n"
     else:
         mystring += "crosskerr= 0.0\n"
-        mystring += "Jkl= " + str(couple_coeff)[1:-1] + "\n"
-    mystring += "collapse_type=none\n"
+    if len(Jkl)>0:
+        mystring += "Jkl= " + str(Jkl)[1:-1] + "\n"
+    else:
+        mystring += "Jkl= 0.0\n"
+    decay = False
+    if len(T1) > 0: 
+        decay = True
+        mystring += "decay_time = " + str(T1)[1:-1] + "\n"
+    dephase = False
+    if len(T2) > 0:
+        dephase = True
+        mystring += "dephase_time = " + str(T2)[1:-1] + "\n"
+    if decay and dephase:
+        mystring += "collapse_type = both\n"
+    elif decay:
+        mystring += "collapse_type = decay\n"
+    elif dephase:
+        mystring += "collapse_type = dephase\n"
+    else:
+        mystring += "collapse_type = none\n"
     mystring += "initialcondition=basis\n"
 
     # choose between having splines for both the real & imaginary parts, or only for the amplitude with a fixed phase
     for iosc in range(1, len(Ne) + 1):
-        if splines_real_imag:
-            mystring += "control_segments" + str(iosc - 1) + " = spline, " + str(D1) + "\n"
-        else:
-            mystring += "control_segments" + str(iosc - 1) + " = spline_amplitude, " + str(D1) + ", " + str(
-                phase_scaling_factor) + "\n"
+        mystring += "control_segments" + str(iosc - 1) + " = spline, " + str(D1) + "\n"
         mystring += "control_initialization" + str(iosc - 1) + " = file, ./" + initialpcof_filename + "\n"
         mystring += "control_bounds" + str(iosc - 1) + " = " + str(optim_bounds[iosc - 1]) + "\n"
         mystring += "carrier_frequency" + str(iosc - 1) + " = "
         omi = carrierfreq[iosc - 1]
-        mystring += ", ".join(str(omi[j] / (2 * np.pi)) for j in range(len(omi))) + "\n"
-
+        for j in range(len(omi)):
+            mystring += str(omi[j]) + ", " 
+        mystring += "\n"
     mystring += "optim_target = gate, fromfile, " + gatefilename + "\n"
     if final_objective == 1:
         mystring += "optim_objective = Jtrace\n"
     elif final_objective == 2:
         mystring += "optim_objective = Jfrobenius\n"
+    elif final_objective == 3:
+        mystring += "optim_objective = Jmeasure\n"
+    else:
+        print("ERROR: Objective type not defined\n")
+        stop
     mystring += "gate_rot_freq = 0.0\n"
     mystring += "optim_weights= 1.0\n"
     mystring += "optim_atol= 1e-5\n"
@@ -58,12 +85,15 @@ def write_Quandary_config_file(configfilename, Nt, Ne, T, nsteps, freq01, rotfre
     mystring += "output_frequency = " + str(nsteps) + "\n"
     mystring += "optim_monitor_frequency = " + str(print_frequency_iter) + "\n"
     mystring += "runtype = " + runtype + "\n"
-    mystring += "usematfree = true\n"
+    if len(Ne) < 6:
+        mystring += "usematfree = true\n"
+    else:
+        mystring += "usematfree = false\n"
     mystring += "linearsolver_type = gmres\n"
     mystring += "linearsolver_maxiter = 20\n"
-    mystring += "np_init = " + str(np.prod(Ne)) + "\n"
 
-    with open(configfilename, "w") as file:
+    outpath = datadir+"/"+configfilename
+    with open(outpath, "w") as file:
         file.write(mystring)
 
-    #print("Quandary config file:", configfilename)
+    print("Quandary config file:", outpath)
