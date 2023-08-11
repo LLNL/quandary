44eb5c633a9847af14b63a2946cec343a3bb7c7a
Stefanie Guenther <guenther5@llnl.gov>
2023-08-11 16:19:02 -0700

    Adding more comments.

--- pythoninterface/example.py
+++ pythoninterface/example.py
@@ -23,23 +23,23 @@ T = 100.0
 # Number of points to resolve the shortest period of the dynamics
 Pmin = 40  # 60 # 40 # 80
 
-# Bounds on the control pulse (in rotational frame, p and q) 
+# Bounds on the control pulse (in rotational frame, p and q) [MHz] 
 maxctrl_MHz = [10.0]  
-# Bspline spacing (ns). The number of Bsplines is T/dtau + 2.
+# Bspline spacing (ns) for control pulse parameterization. // The number of Bspline basis functions is then T/dtau + 2.
 dtau = 3.33
 
-# Amplitude of initial (randomized) control vector
+# Set the amplitude of initial (randomized) control vector
 amp_frac = 0.9
-initctrl_MHz = [amp_frac * maxctrl_MHz[i] for i in range(len(maxctrl_MHz))]
+initctrl_MHz = [amp_frac * maxctrl_MHz[i] for i in range(len(Ne))]
 rand_seed = 1234
 randomize_init_ctrl = True
 
-# Set up a target gate 
-unitary = [[0,0,1],[0,1,0],[1,0,0]]  # Swap first and last level
+# Set up a target gate (in essential level dimensions)
+unitary = [[0,0,1],[0,1,0],[1,0,0]]  # Swaps first and last level
 # print(unitary)
 
 # Optimization options
-costfunction = "Jtrace"
+costfunction = "Jtrace"     # "Jtrace", "Jfrobenius"
 initialcondition = "basis"  # "basis", "diagonal", "pure, 0,0,1,...", "file, /path/to/file" 
 gamma_tik0 = 1e-4 	# Tikhonov regularization
 gamma_energy = 0.01	# Penality: Integral over control pulse energy
@@ -48,17 +48,17 @@ tol_costfunc = 1e-3	# Stopping criterion based on the objective function
 maxiter = 100 		# Maximum number of optimization iterations
 
 # Quandary run options
-runtype = "simulation"  # "simulation", or "gradient", or "optimization"
+runtype = "optimization"  # "simulation", or "gradient", or "optimization"
 quandary_exec="/Users/guenther5/Numerics/quandary/main"
 ncores = np.prod(Ne)  # Number of cores 
 # ncores = 1
 datadir = "./run_dir"  # Compute and output directory 
-verbose = False
+verbose = True
 
-# # Load pcof0 from file
-with open('./params.dat', 'r') as f:
-    pcof0 = [float(line.strip()) for line in f if line]
-# pcof0=[]
+# Potentially load initial control parameters from a file
+# with open('./params.dat', 'r') as f:
+#     pcof0 = [float(line.strip()) for line in f if line]
+pcof0=[]
 
 # Execute quandary
 popt, infidelity, optim_hist = pulse_gen(Ne, Ng, freq01, selfkerr, crosskerr, Jkl, rotfreq, maxctrl_MHz, T, initctrl_MHz, rand_seed, randomize_init_ctrl, unitary,  dtau=dtau, Pmin=Pmin, datadir=datadir, tol_infidelity=tol_infidelity, tol_costfunc=tol_costfunc, maxiter=maxiter, gamma_tik0=gamma_tik0, gamma_energy=gamma_energy, costfunction=costfunction, initialcondition=initialcondition, T1=T1, T2=T2, runtype=runtype, quandary_exec=quandary_exec, ncores=ncores, verbose=verbose, pcof0=pcof0)
