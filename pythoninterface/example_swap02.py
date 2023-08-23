07827a184a5acfc138a3d74986072ff05dda2489
Stefanie Guenther <guenther5@llnl.gov>
2023-08-23 15:36:49 -0700

    Updating examples for new config dataclass.

--- pythoninterface/example_swap02.py
+++ pythoninterface/example_swap02.py
@@ -21,10 +21,6 @@ maxctrl_MHz = [10.0]
 unitary = [[0,0,1],[0,1,0],[1,0,0]]  # Swaps first and last level
 # print(unitary)
 
-# # Potentially create custom Hamiltonian model, if different from standard model
-# Ntotal = [sum(x) for x in zip(Ne, Ng)]
-# Hsys, Hc_re, Hc_im = hamiltonians(Ntotal, freq01, selfkerr, crosskerr, Jkl, rotfreq=rotfreq, verbose=True)
-
 # Potentially load initial control parameters from a file. Use absolute path!
 # pcof0_filename = os.getcwd() + "/SWAP02_params.dat"
 # pcof0=np.zeros(30)
@@ -37,7 +33,7 @@ quandary_exec="/Users/guenther5/Numerics/quandary/main"
 verbose = True
 
 # Prepare Quandary
-myconfig = QuandaryConfig(Ne=Ne, Ng=Ng, freq01=freq01, selfkerr=selfkerr, rotfreq=rotfreq, maxctrl_MHz=maxctrl_MHz, targetgate=unitary, verbose=verbose)
+myconfig = QuandaryConfig(Ne=Ne, Ng=Ng, freq01=freq01, selfkerr=selfkerr, rotfreq=rotfreq, maxctrl_MHz=maxctrl_MHz, targetgate=unitary, T=T, verbose=verbose)
 
 # Execute quandary
 time, pt, qt, ft, expectedEnergy, popt, infidelity, optim_hist = quandary_run(myconfig, quandary_exec=quandary_exec)
@@ -53,19 +49,13 @@ if False:
 
 # TODO:
 #   * All function call arguments should be keyword only.  
-#   * Switch between standard model and self-created Hamiltonian operators -> modify pulse_gen arguments
 #   * Create high-level functions for pulse_gen vs simulation
-#   * Change return of pulse_gen to match Tensorflow returns
+#   * Match input/return arguments of pulse_gen to Tensorflow args
 #   * Ander: Get resonances: Iterating over non-zeros of U'HcU, should be all elements? Previously it was only lower triangular matrix, but it is not hermitian, so this makes a difference!
 #   * Anders: Culled and sorted carrier waves, is that needed? 
     #   # CNOT case with Jkl coupling is not converging!
 #   * Add custom decoherence operators to Quandary? 
-#   * Gather all configuration in a dictionary (or other struct) that contains all defaults and allows for changes. Dump it with 
-    # with open("config.cfg", 'w') as f:
-    #     for key, value in config_dict.items():
-    #         f.write('%s = %s' %(key, value))
 
 # Note: 
-#   * pcof0 uses Quandaries initialization
 #   * leakage_weights = [0.0, 0.0] is disabled.
 #   * "use_eigenbasis" disabled.
