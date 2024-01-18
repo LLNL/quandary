a44b65fd2d385929ce2556b619ef7795df091885
Anders Petersson <petersson1@llnl.gov>
2024-01-17 17:15:18 -0800

    Adjusting the number of time steps, commenting out optim_penalty_dpdm, and adding optim_mu parameter option

---
+++ cases/plot_conv.jl
@@ -0,0 +1,14 @@
+using Plots
+using FileIO
+using DelimitedFiles
+
+d, h = readdlm("optim_history.dat",header=true)
+
+pl = plot(d[:,1],d[:,2],lab="Obj",yscale=:log10,xlabel="Iterations")
+plot!(d[:,1],d[:,3],lab="Norm(grad)",yscale=:log10)
+plot!(d[:,1],d[:,6],lab="Infid",yscale=:log10)
+plot!(d[:,1],d[:,7],lab="Tik",yscale=:log10)
+plot!(d[:,1],d[:,10],lab="Ctrl Energy",yscale=:log10)
+plot!(d[:,1],d[:,11].+1e-10,lab="Discont",yscale=:log10)
+
+ylims!(1e-6,30)
