06704bc68a1df23d9bc63a18af215d2041f81d89
Anders Petersson <petersson1@llnl.gov>
2024-01-25 09:27:31 -0800

    Adjusting parameters for the QFT test cases and updating the plot script for the convergence results

--- cases/plot_conv.jl
+++ cases/plot_conv.jl
@@ -5,9 +5,9 @@ using DelimitedFiles
 d, h = readdlm("optim_history.dat",header=true)
 
 pl = plot(d[:,1],d[:,2],lab="Obj",yscale=:log10,xlabel="Iterations")
-plot!(d[:,1],d[:,3],lab="Norm(grad)",yscale=:log10)
 plot!(d[:,1],d[:,6],lab="Infid",yscale=:log10)
-plot!(d[:,1],d[:,7],lab="Tik",yscale=:log10)
+plot!(d[:,1],d[:,3],lab="Norm(grad)",yscale=:log10)
+plot!(d[:,1],d[:,7],lab="Tikonov",yscale=:log10)
 plot!(d[:,1],d[:,10],lab="Ctrl Energy",yscale=:log10)
 plot!(d[:,1],d[:,11].+1e-10,lab="Discont",yscale=:log10)
 
