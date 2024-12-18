5334a9fe0587685c28e97bad56a4314f60c7ecb4
Tara Drwenski <drwenski1@llnl.gov>
2024-12-18 10:20:05 -0700

    Add echos to build script

--- .gitlab/build_and_test.sh
+++ .gitlab/build_and_test.sh
@@ -2,9 +2,13 @@
 
 set -e
 
+echo "~~~~~~~~~~ START:build_and_test.sh ~~~~~~~~~~~"
+
 export PETSC_DIR=/usr/tce/packages/petsc/petsc-3.18.3
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PETSC_DIR/lib
 
 make cleanup
 make quandary
 ./tests/runRegressionTests.sh
+
+echo "~~~~~~~~~~ END:build_and_test.sh ~~~~~~~~~~~~~"
