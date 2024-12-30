0b37ef8f2123d4e160bb3e88d8691f10f1ce49d7
Tara Drwenski <drwenski1@llnl.gov>
2024-12-30 14:50:32 -0700

    Port bash test runner to pytest

---
+++ tests/regression_test.py
@@ -0,0 +1,52 @@
+import glob
+import pytest
+import os
+import subprocess
+
+from compare_two_files import compare_two_files
+
+TOL = 1.0e-7
+TEST_DIR = os.path.dirname(os.path.realpath(__file__))
+QUANDARY = os.path.join(TEST_DIR, "..", "quandary")
+
+BASE_TEST_SET = ["grad.dat", "optim_history.dat"]
+EXTENDED_TEST_SET = BASE_TEST_SET + ["rho*.dat", "population*.dat"]
+
+TEST_CASES = [
+    ("AxC", EXTENDED_TEST_SET),
+    ("AxC_detuning", BASE_TEST_SET),
+    ("AxC_groundstate", BASE_TEST_SET),
+    ("AxC_initBasis0", BASE_TEST_SET),
+    ("AxC_initDiag0", BASE_TEST_SET),
+    ("AxC_initFile", BASE_TEST_SET),
+    ("AxC_schroedinger_matfree", BASE_TEST_SET),
+    ("cnot", EXTENDED_TEST_SET),
+    ("cnot-schroedinger", BASE_TEST_SET),
+    ("hadamard", BASE_TEST_SET),
+    ("pipulse", EXTENDED_TEST_SET),
+    ("qubit", BASE_TEST_SET),
+    ("xgate", EXTENDED_TEST_SET),
+    ("ygate", BASE_TEST_SET),
+    ("zgate", BASE_TEST_SET),
+]
+
+
+@pytest.mark.parametrize("simulation_name,files_to_compare", TEST_CASES)
+def test_eval(simulation_name, files_to_compare):
+    simulation_dir = os.path.join(TEST_DIR, simulation_name)
+    config_file = os.path.join(simulation_dir, simulation_name + ".cfg")
+
+    os.chdir(simulation_dir)
+    command = [QUANDARY, config_file]
+    result = subprocess.run(command, capture_output=True, text=True, check=True)
+    assert result.returncode == 0
+
+    matching_files = [file for pattern in files_to_compare for file in glob.glob(os.path.join(simulation_dir, "base", pattern))]
+    for expected in matching_files:
+        file_name = os.path.basename(expected)
+        output = os.path.join(simulation_dir, "data_out", file_name)
+        compare_files_old(output, expected)
+
+
+def compare_files_old(output, expected):
+    compare_two_files(expected, output, TOL, 0)
