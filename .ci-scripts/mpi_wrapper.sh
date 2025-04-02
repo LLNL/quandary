4c4039752948f2e3aff592eb9ad0736a6dee961c
Tara Drwenski <drwenski1@llnl.gov>
2025-04-02 16:08:45 -0600

    codespace fixes

--- .ci-scripts/mpi_wrapper.sh
+++ .ci-scripts/mpi_wrapper.sh
@@ -2,6 +2,9 @@
 
 mkdir -p /root/bin
 MPI_PATH=$(command -v mpirun)
-echo -e "#!/bin/bash\nexec $MPI_PATH --map-by :OVERSUBSCRIBE --allow-run-as-root \"\$@\"" > /root/bin/mpirun
+cat > /root/bin/mpirun << EOF
+#!/bin/bash
+exec $MPI_PATH --map-by :OVERSUBSCRIBE --allow-run-as-root "\$@"
+EOF
 chmod +x /root/bin/mpirun
 echo 'export PATH="/root/bin:$PATH"' >> ~/.bashrc
