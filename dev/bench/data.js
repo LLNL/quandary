f68f6df29a6e3b8c9d94eeb91d5f4fa0c7eda0cf
github-action-benchmark <github@users.noreply.github.com>
2025-04-25 00:12:19 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for da5a77d076f0a812a8b5694c82f72cb16df314f7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745535644309,
+  "lastUpdate": 1745539939689,
   "repoUrl": "https://github.com/LLNL/quandary",
   "entries": {
     "Benchmark": [
@@ -61,6 +61,68 @@ window.BENCHMARK_DATA = {
             "extra": {
               "number_of_processors": 4,
               "memory_mb": 163.54
+            }
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Tara Drwenski",
+            "username": "tdrwenski",
+            "email": "drwenski1@llnl.gov"
+          },
+          "committer": {
+            "name": "Tara Drwenski",
+            "username": "tdrwenski",
+            "email": "drwenski1@llnl.gov"
+          },
+          "id": "da5a77d076f0a812a8b5694c82f72cb16df314f7",
+          "message": "Revert custom pytest hook and put benchmark format conversion in own file",
+          "timestamp": "2025-04-24T22:48:06Z",
+          "url": "https://github.com/LLNL/quandary/commit/da5a77d076f0a812a8b5694c82f72cb16df314f7"
+        },
+        "date": 1745539939245,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "test_eval[config_template_1] - Time",
+            "value": 3.296414813795127,
+            "unit": "seconds",
+            "range": "0.6155023926807012",
+            "extra": {
+              "number_of_processors": 1,
+              "memory_mb": 39.16
+            }
+          },
+          {
+            "name": "test_eval[config_template_1] - Memory",
+            "value": 39.16,
+            "unit": "MB",
+            "range": "0",
+            "extra": {
+              "number_of_processors": 1,
+              "memory_mb": 39.16
+            }
+          },
+          {
+            "name": "test_eval[config_template_4] - Time",
+            "value": 1.2689366455888376,
+            "unit": "seconds",
+            "range": "0.04634240184557715",
+            "extra": {
+              "number_of_processors": 4,
+              "memory_mb": 164.23
+            }
+          },
+          {
+            "name": "test_eval[config_template_4] - Memory",
+            "value": 164.23,
+            "unit": "MB",
+            "range": "0",
+            "extra": {
+              "number_of_processors": 4,
+              "memory_mb": 164.23
             }
           }
         ]
