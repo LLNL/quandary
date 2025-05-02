9c401966d6c7199ac8ef88e109d2b0f2479ab30b
github-action-benchmark <github@users.noreply.github.com>
2025-05-02 20:29:50 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for a01d6d1bad69e654ee8ef38f1101354987e7ecef

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1746215497956,
+  "lastUpdate": 1746217790271,
   "repoUrl": "https://github.com/LLNL/quandary",
   "entries": {
     "Benchmark": [
@@ -87,6 +87,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "test_eval[nlevels_32_32_32_32_nproc_32] - Memory",
             "value": 5037.7,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Tara Drwenski",
+            "username": "tdrwenski",
+            "email": "drwenski1@llnl.gov"
+          },
+          "committer": {
+            "name": "Tara Drwenski",
+            "username": "tdrwenski",
+            "email": "drwenski1@llnl.gov"
+          },
+          "id": "a01d6d1bad69e654ee8ef38f1101354987e7ecef",
+          "message": "Update test names",
+          "timestamp": "2025-05-02T19:26:14Z",
+          "url": "https://github.com/LLNL/quandary/commit/a01d6d1bad69e654ee8ef38f1101354987e7ecef"
+        },
+        "date": 1746217789748,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "test_eval[nlevels_4_4_4_4_nproc_16] - Time",
+            "value": 0.8020379805198172,
+            "unit": "seconds",
+            "range": "0.37997492686402373"
+          },
+          {
+            "name": "test_eval[nlevels_4_4_4_4_nproc_16] - Memory",
+            "value": 882.15,
+            "unit": "MB"
+          },
+          {
+            "name": "test_eval[nlevels_32_32_32_32_nproc_32] - Time",
+            "value": 19.63581712319283,
+            "unit": "seconds",
+            "range": "0.19583773102626836"
+          },
+          {
+            "name": "test_eval[nlevels_32_32_32_32_nproc_32] - Memory",
+            "value": 5040.83,
             "unit": "MB"
           }
         ]
