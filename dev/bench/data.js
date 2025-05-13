cde6da74238d2e9b9a133578e6cdc17cf05757cd
github-action-benchmark <github@users.noreply.github.com>
2025-05-13 23:02:18 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for fc94d74eb6799cf22204e816d0d5ff3fdd0f17f9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747152934851,
+  "lastUpdate": 1747177338485,
   "repoUrl": "https://github.com/LLNL/quandary",
   "entries": {
     "Benchmark": [
@@ -87,6 +87,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "test_eval[nlevels_32_32_32_32_nproc_32] - Memory",
             "value": 5039.89,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Stefanie Guenther",
+            "username": "steffi7574",
+            "email": "guenther5@llnl.gov"
+          },
+          "committer": {
+            "name": "Stefanie Guenther",
+            "username": "steffi7574",
+            "email": "guenther5@llnl.gov"
+          },
+          "id": "fc94d74eb6799cf22204e816d0d5ff3fdd0f17f9",
+          "message": "Fix Readme .",
+          "timestamp": "2025-05-13T22:12:12Z",
+          "url": "https://github.com/LLNL/quandary/commit/fc94d74eb6799cf22204e816d0d5ff3fdd0f17f9"
+        },
+        "date": 1747177337456,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "test_eval[nlevels_4_4_4_4_nproc_2] - Time",
+            "value": 1.2243254836807318,
+            "unit": "seconds",
+            "range": "0.2855656481640038"
+          },
+          {
+            "name": "test_eval[nlevels_4_4_4_4_nproc_2] - Memory",
+            "value": 90.52,
+            "unit": "MB"
+          },
+          {
+            "name": "test_eval[nlevels_32_32_32_32_nproc_32] - Time",
+            "value": 19.701801151200197,
+            "unit": "seconds",
+            "range": "0.24164525478795115"
+          },
+          {
+            "name": "test_eval[nlevels_32_32_32_32_nproc_32] - Memory",
+            "value": 5039.85,
             "unit": "MB"
           }
         ]
