dbf4a7ad69be0810e2633c59f18c65542ad840be
steffi7574 <steffi7574@users.noreply.github.com>
2025-06-11 22:43:42 +0000

    deploy: 3120938ae4a5eaf0e16c33abe11356d571b643ba

--- doxygen/search/functions_12.js
+++ doxygen/search/functions_12.js
@@ -1,18 +1,19 @@
 var searchData=
 [
   ['sanitytests_0',['sanitytests',['../util_8hpp.html#af937251d32a8fc477f20e485b185a035',1,'SanityTests(Vec x, PetscReal time):&#160;util.hpp'],['../util_8cpp.html#ab4a4256c5bd737d676b10694734500f6',1,'SanityTests(Vec x, double time):&#160;util.cpp']]],
-  ['setcontrolamplitudes_1',['setControlAmplitudes',['../classMasterEq.html#af747a9c4013860f785cd80fe52e7f457',1,'MasterEq']]],
-  ['setparams_2',['setParams',['../classOscillator.html#ac9de47930cb8adf8c34280ec9720ee8c',1,'Oscillator']]],
-  ['setskip_3',['setSkip',['../classControlBasis.html#ab1b24c656aec7d1eef58f173620d0135',1,'ControlBasis']]],
-  ['sigmoid_4',['sigmoid',['../util_8cpp.html#a653c752340de134fef06013d3869bd3a',1,'sigmoid(double width, double x):&#160;util.cpp'],['../util_8hpp.html#a653c752340de134fef06013d3869bd3a',1,'sigmoid(double width, double x):&#160;util.cpp']]],
-  ['sigmoid_5fdiff_5',['sigmoid_diff',['../util_8cpp.html#a2033762ab1a7f716ade76814839aa7e0',1,'sigmoid_diff(double width, double x):&#160;util.cpp'],['../util_8hpp.html#a2033762ab1a7f716ade76814839aa7e0',1,'sigmoid_diff(double width, double x):&#160;util.cpp']]],
-  ['solve_6',['solve',['../classOptimProblem.html#af8a76f97039d415b79a7bf9fd525c78a',1,'OptimProblem']]],
-  ['solveadjointode_7',['solveAdjointODE',['../classTimeStepper.html#aab958d4087d5b20f0f6f6695560d05c4',1,'TimeStepper']]],
-  ['solveode_8',['solveODE',['../classTimeStepper.html#ae2be974cc2dd652299ba12a36ffece58',1,'TimeStepper']]],
-  ['statehastrace1_9',['statehastrace1',['../util_8cpp.html#a7c1b5f8c4eca88aff3fec5cfbb42ffd1',1,'StateHasTrace1(Vec x, PetscReal tol, PetscBool *flag):&#160;util.cpp'],['../util_8hpp.html#a7c1b5f8c4eca88aff3fec5cfbb42ffd1',1,'StateHasTrace1(Vec x, PetscReal tol, PetscBool *flag):&#160;util.cpp']]],
-  ['stateishermitian_10',['stateishermitian',['../util_8cpp.html#a6bd4be55b2c06de010e63e1d5b8a3077',1,'StateIsHermitian(Vec x, PetscReal tol, PetscBool *flag):&#160;util.cpp'],['../util_8hpp.html#a6bd4be55b2c06de010e63e1d5b8a3077',1,'StateIsHermitian(Vec x, PetscReal tol, PetscBool *flag):&#160;util.cpp']]],
-  ['step_11',['Step',['../classStep.html#a57602386460f018e9c712880556b1c15',1,'Step']]],
-  ['stringtrim_12',['StringTrim',['../config_8cpp.html#a415263e1b50197687f0ba572a59fa66f',1,'config.cpp']]],
-  ['swap_13',['SWAP',['../classSWAP.html#ad17bf33ddf20dc890aa5c455eee4c2b0',1,'SWAP']]],
-  ['swap_5f0q_14',['SWAP_0Q',['../classSWAP__0Q.html#aab439722024ac3e5a8f43af1bd259744',1,'SWAP_0Q']]]
+  ['set_5frhs_5fmatmult_5foperation_1',['set_RHS_MatMult_operation',['../classMasterEq.html#a9e3fcf025c5e78f7cd5bed77d48660a7',1,'MasterEq']]],
+  ['setcontrolamplitudes_2',['setControlAmplitudes',['../classMasterEq.html#af747a9c4013860f785cd80fe52e7f457',1,'MasterEq']]],
+  ['setparams_3',['setParams',['../classOscillator.html#ac9de47930cb8adf8c34280ec9720ee8c',1,'Oscillator']]],
+  ['setskip_4',['setSkip',['../classControlBasis.html#ab1b24c656aec7d1eef58f173620d0135',1,'ControlBasis']]],
+  ['sigmoid_5',['sigmoid',['../util_8hpp.html#a653c752340de134fef06013d3869bd3a',1,'sigmoid(double width, double x):&#160;util.cpp'],['../util_8cpp.html#a653c752340de134fef06013d3869bd3a',1,'sigmoid(double width, double x):&#160;util.cpp']]],
+  ['sigmoid_5fdiff_6',['sigmoid_diff',['../util_8hpp.html#a2033762ab1a7f716ade76814839aa7e0',1,'sigmoid_diff(double width, double x):&#160;util.cpp'],['../util_8cpp.html#a2033762ab1a7f716ade76814839aa7e0',1,'sigmoid_diff(double width, double x):&#160;util.cpp']]],
+  ['solve_7',['solve',['../classOptimProblem.html#af8a76f97039d415b79a7bf9fd525c78a',1,'OptimProblem']]],
+  ['solveadjointode_8',['solveAdjointODE',['../classTimeStepper.html#aab958d4087d5b20f0f6f6695560d05c4',1,'TimeStepper']]],
+  ['solveode_9',['solveODE',['../classTimeStepper.html#ae2be974cc2dd652299ba12a36ffece58',1,'TimeStepper']]],
+  ['statehastrace1_10',['statehastrace1',['../util_8cpp.html#a7c1b5f8c4eca88aff3fec5cfbb42ffd1',1,'StateHasTrace1(Vec x, PetscReal tol, PetscBool *flag):&#160;util.cpp'],['../util_8hpp.html#a7c1b5f8c4eca88aff3fec5cfbb42ffd1',1,'StateHasTrace1(Vec x, PetscReal tol, PetscBool *flag):&#160;util.cpp']]],
+  ['stateishermitian_11',['stateishermitian',['../util_8cpp.html#a6bd4be55b2c06de010e63e1d5b8a3077',1,'StateIsHermitian(Vec x, PetscReal tol, PetscBool *flag):&#160;util.cpp'],['../util_8hpp.html#a6bd4be55b2c06de010e63e1d5b8a3077',1,'StateIsHermitian(Vec x, PetscReal tol, PetscBool *flag):&#160;util.cpp']]],
+  ['step_12',['Step',['../classStep.html#a57602386460f018e9c712880556b1c15',1,'Step']]],
+  ['stringtrim_13',['StringTrim',['../config_8cpp.html#a415263e1b50197687f0ba572a59fa66f',1,'config.cpp']]],
+  ['swap_14',['SWAP',['../classSWAP.html#ad17bf33ddf20dc890aa5c455eee4c2b0',1,'SWAP']]],
+  ['swap_5f0q_15',['SWAP_0Q',['../classSWAP__0Q.html#aab439722024ac3e5a8f43af1bd259744',1,'SWAP_0Q']]]
 ];
