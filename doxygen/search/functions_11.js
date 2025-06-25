670ce7c88104d7d9a0590944065d71d62fe12a57
steffi7574 <steffi7574@users.noreply.github.com>
2025-06-25 17:20:00 +0000

    deploy: 4e539a4db385f3db2fa2a6e505e3ee0088e8acf5

--- doxygen/search/functions_11.js
+++ doxygen/search/functions_11.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['read_5fvector_0',['read_vector',['../util_8cpp.html#afcffc7f23e5d34c4a4b267eb5105c417',1,'read_vector(const char *filename, double *var, int dim, bool quietmode, int skiplines, const std::string testheader):&#160;util.cpp'],['../util_8hpp.html#ab3ac00c89b66ff90bcfb542e20df3dd0',1,'read_vector(const char *filename, double *var, int dim, bool quietmode=false, int skiplines=0, const std::string testheader=&quot;&quot;):&#160;util.cpp']]],
   ['readfile_1',['ReadFile',['../classConfig.html#a2c5e36161681692c9094eccdffa03617',1,'Config']]],
-  ['receivehc_2',['receiveHc',['../classHamiltonianFileReader.html#aa89bb7b20217ce23e1d884e02ab4e220',1,'HamiltonianFileReader']]],
-  ['receivehsys_3',['receiveHsys',['../classHamiltonianFileReader.html#a3e6179396421e3bff256c2996e44ae1d',1,'HamiltonianFileReader']]]
+  ['receivehc_2',['receiveHc',['../classHamiltonianFileReader.html#a1f282ed6b03d5812db0cca79afe783fe',1,'HamiltonianFileReader']]],
+  ['receivehsys_3',['receiveHsys',['../classHamiltonianFileReader.html#a9d8e4ff8fb0574f2fe3f04b1e1841df6',1,'HamiltonianFileReader']]]
 ];
--- doxygen/search/variables_7.js
+++ doxygen/search/variables_7.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['hamiltonian_5ffile_0',['hamiltonian_file',['../classHamiltonianFileReader.html#a575a2f47d5a45c4a56cae5af2f31d382',1,'HamiltonianFileReader::hamiltonian_file'],['../classMasterEq.html#a68172dcac6206bd43927af847b87b822',1,'MasterEq::hamiltonian_file']]]
+  ['hamiltonian_5ffile_5fhc_0',['hamiltonian_file_hc',['../classHamiltonianFileReader.html#a7b71c5dbff1375a97ba113183841b16e',1,'HamiltonianFileReader::hamiltonian_file_Hc'],['../classMasterEq.html#a4cc4d6da9ba0169c505d9a003e29a3fe',1,'MasterEq::hamiltonian_file_Hc']]],
+  ['hamiltonian_5ffile_5fhsys_1',['hamiltonian_file_hsys',['../classHamiltonianFileReader.html#a75f0d06fa76ab5f3dc730e1cb8a185ca',1,'HamiltonianFileReader::hamiltonian_file_Hsys'],['../classMasterEq.html#a7b3188a199da02dba7d150c383b3f460',1,'MasterEq::hamiltonian_file_Hsys']]]
 ];
