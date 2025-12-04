ea0d7c55b9f7b91c086dc644cbfd676bd90db846
Tara Drwenski <drwenski1@llnl.gov>
2025-12-03 18:22:42 -0800

    Add barrier to ensure data_dir is created

--- src/output.cpp
+++ src/output.cpp
@@ -29,6 +29,7 @@ Output::Output(const Config& config, MPI_Comm comm_petsc, MPI_Comm comm_init, bo
   if (mpirank_world == 0) {
     mkdir(datadir.c_str(), 0777);
   }
+  MPI_Barrier(MPI_COMM_WORLD);
 
   /* Prepare output for optimizer */
   optim_monitor_freq = config.getOptimMonitorFrequency();
